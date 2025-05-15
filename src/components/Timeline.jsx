55ef3425404c4ff8cf792726449b8cc6c001b795
Ashish Sethi <131164385+ashishsethi-SM@users.noreply.github.com>
2025-05-15 23:01:50 +0530

    feat: added error handling states

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,7 +3,6 @@ import Loading from './Loading'
 import Post from './Post'
 
 const Timeline = ({ postsData, filteredPosts }) => {
-	console.log('FILTERED data', filteredPosts)
 	const topPosts = useMemo(() => {
 		return [...filteredPosts]
 			.filter((post) => post.socialActivityCountsInsight?.totalReactionCount)
@@ -18,6 +17,10 @@ const Timeline = ({ postsData, filteredPosts }) => {
 		<div className='flex flex-col items-center gap-4'>
 			{postsData.loading ? (
 				<Loading />
+			) : postsData.error ? (
+				<p className='text-blue-500 font-semibold'>
+					An error occurred: {postsData.error || 'Unable to load posts.'}
+				</p>
 			) : topPosts && topPosts.length > 0 ? (
 				topPosts.map((post) => <Post post={post} key={post.urn} />)
 			) : (
